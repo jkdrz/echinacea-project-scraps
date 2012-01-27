007fbd266975998e0b904f7850e902957f6bd0d3
Josh Drizin <josh@respectablename.com>
2012-01-27 13:32:15 -0600

    dropped the exe column, since that's windows-specific and not very useful anyway

--- dir.ea.r
+++ dir.ea.r
@@ -6,7 +6,7 @@ dir.ea <- function(path = ".") {
    if(path == ".") path <- getwd()
    dir <- file.info(list.files(path, full.names = FALSE, recursive = TRUE, include.dirs = FALSE))
    dir$fileName <- row.names(dir)
-   dir <- dir[ , c(8, 1:7)] #this is windows specific. won't work on POSIX-like systems.
+   dir <- dir[ , c("fileName", "size", "isdir", "mode", "mtime", "ctime")] #dropped exe for compat.
    row.names(dir) <- NULL
    date <- Sys.time()
    ans <- list(dir = dir,
