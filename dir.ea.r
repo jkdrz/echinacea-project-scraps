9868c36948a30cdd0b0ef9261a32598e5bcff5ba
Josh Drizin <josh@respectablename.com>
2012-01-27 13:01:02 -0600

    commit the version of dir.ea() on cohuman, as stuart initially posted it.

---
+++ dir.ea.r
@@ -0,0 +1,36 @@
+# function dir.ea extracts information about files and returns
+# the path accessed, the time accessed and a data frame,
+##############
+
+dir.ea <- function(path = ".") {
+   if(path == ".") path <- getwd()
+   dir <- file.info(list.files(path, full.names = FALSE, recursive = TRUE, include.dirs = FALSE))
+   dir$fileName <- row.names(dir)
+   dir <- dir[ , c(8, 1:7)]
+   row.names(dir) <- NULL
+   date <- Sys.time()
+   ans <- list(dir = dir,
+               path = path,
+               date = date
+               )
+   name <- paste(dirname(path), "/", basename(path), "/", "dir-", basename(path), "-", format(date, "%Y-%b-%d"), ".csv", sep = "")
+   ans
+}
+
+# Examples
+
+getwd()
+dir.ea()
+
+path <- getwd()
+dir.ea(path)
+
+foo <- dir.ea(path)
+foo$path
+foo$date
+
+dir.ea("I:/Departments/Research/Wagenius/Stuart's Reprints/Trager1998")
+
+setwd(choose.dir())
+
+getwd()
