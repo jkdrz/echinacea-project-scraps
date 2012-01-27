10c946985b153ea0e65742c95a7ac8a4ba9f7d99
Josh Drizin <josh@respectablename.com>
2012-01-27 13:15:49 -0600

    first commit of a new bit of code. this will compare filenames.

---
+++ dirCompare.r
@@ -0,0 +1,3 @@
+# the idea is to eventually use dir.ea() to pull what's in a directory and compare
+# to a vector containing the letnos or filenames that should be there.
+# some finessing of filenames or letnos may be required.
