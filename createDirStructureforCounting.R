8ff97ec57032ad3cb681fa4bc15159fe0302e9df
Josh Drizin <josh+cbg@respectablename.com>
2012-04-04 11:54:42 -0500

    letnos are assigned to directories.

--- createDirStructureforCounting.R
+++ createDirStructureforCounting.R
@@ -5,6 +5,9 @@
 ####################################################
 #todo:                                             #
 #figure out filename case sensitivity issues       #
+#not every filename is in lowercase, which might   #
+#end up being problematic, but i'm not sure if R   #
+#is case-sensitive when it runs through system()   #
 ####################################################
 
 #import harvest list, fix letnos (code from scanCompare2012.R)
@@ -12,4 +15,19 @@ harvList <- read.csv("I:\\\\Departments\\Research\\EchinaceaCG2011\\2011.CG1.Har
 harvList$letnoHarv <- as.character(harvList$letnoHarv) #you need to make these character vectors
 harvList$letnoCorrected <- as.character(harvList$letnoCorrected)
 harvList$letnoHarv[complete.cases(harvList$letnoCorrected)] <- harvList$letnoCorrected[complete.cases(harvList$letnoCorrected)]
+harvList <- harvList[harvList$gBagCorrected != "NH" , ]
 
+#import the happy word list: we want the lowercase vector
+happy <- read.csv("I:\\\\Departments\\Research\\EchinaceaCG2011\\happyWords.csv")
+
+#figure out how many records we have and how many directories we need
+length(harvList$letnoHarv) #2892 files, at the top end. 6 in each dir, 482 directories
+
+#we'll add a few dummy directories, just in case. pull out 490 entries
+happyList <- sample(happy$lowercase, 490, replace=FALSE)
+happyDirs <- rep(happyList, each=6)
+
+# put together the vectors, keeping lets and nos, so i can actually write out files in batches
+randomHarvList <- sample(harvList)
+happyDirs[1:length(randomHarvList$letnoHarv)]
+randomHarvList <- data.frame(randomHarvList, happyDirs[1:length(randomHarvList$letnoHarv)])
