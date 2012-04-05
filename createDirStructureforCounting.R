e8b5576d242d5b5dafcc5bd978bf90073c21de9d
Josh Drizin <josh+cbg@respectablename.com>
2012-04-05 14:15:06 -0500

    moved some code around to keep it together, updated the rest of the script to output the 8000s correctly. this entire script could be run at once.

--- createDirStructureforCounting.R
+++ createDirStructureforCounting.R
@@ -15,7 +15,10 @@ happy <- read.csv("I:\\\\Departments\\Research\\EchinaceaCG2011\\happyWords.csv"
 #figure out how many records we have and how many directories we need
 length(harvList$letnoHarv) #2892 files, at the top end. 6 in each dir, 482 directories
 
+harvList$let <- substr(harvList$letnoHarv, 1, 2)
 harvList$no <- as.integer(substr(harvList$letnoHarv, 4, 7))
+harvList$filename <- paste(harvList$no, harvList$let, ".jpg", sep="")#turn letno into nolet.jpg
+
 inb1and2 <- harvList$no < 9000 & harvList$no >= 8000
 
 h8000 <- harvList[inb1and2, ]
@@ -26,32 +29,28 @@ happyDirs <- rep(happyList, each=6)
 
 # put together the vectors, keeping lets and nos, so i can actually write out files in batches
 
-h8000 <- h8000[sample(1: dim(h8000)[1]), ]
+h8000 <- h8000[sample(1: dim(h8000)[1]), ] #sample the dataframe. sample(h8000) won't work
 happyDirs8000 <- happyDirs[1:230]
 randomHarvList8000 <- data.frame(h8000, happyDirs8000)
-names(randomHarvList8000)[29] <- "happyDirs"
 leftoverHappyDirs <- happyDirs[(length(randomHarvList8000$letnoHarv)+1):length(happyDirs)]
 
 #write out some useful files
-#write.csv(randomHarvList, file="C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.RandomHarvList.csv")
-#write.csv(leftoverHappyDirs, file="C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.LeftoverHappyDirs.csv")
+write.csv(randomHarvList8000, file="C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.HarvList8000.csv")
+write.csv(leftoverHappyDirs, file="C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.LeftoverHappyDirs.csv")
 
 # create directory structure. only run this this first time.
   setwd("C:\\\\cg2011counting\\")
-  dirCommands <- paste("mkdir ", "C:\\cg2011counting\\", unique(randomHarvList$happyDirs), sep="")
+  dirCommands <- paste("mkdir ", "C:\\cg2011counting\\", unique(randomHarvList8000$happyDirs8000), sep="")
   write.table(dirCommands, file="mkdirCommands.bat", sep="", row.names=FALSE, col.names=FALSE, quote=FALSE) 
   #this writes hard paths. find the .bat file and double-click it to run it.
 
 # move files around this uses hard paths, which is ugly, but i think it's the only way
-#turn letno into nolet.jpg
-randomHarvList$no <- as.integer(substr(harvList$letnoHarv, 4, 7))
-randomHarvList$let <- substr(harvList$letnoHarv, 1, 2) #windows is not case sensitive. no problem
-randomHarvList$filename <- paste(harvList$no, harvList$let, ".jpg", sep="")
+
 #put together the copy commands with hard paths. use subsets to do batches manually.
 moveFiles <- paste("copy ", "C:\\2011_scans_sorted\\8000\\", 
-      randomHarvList$filename[randomHarvList$no < 9000 & randomHarvList$no >= 8000], " C:\\\\cg2011counting\\",
-      randomHarvList$happyDirs[randomHarvList$no < 9000 & randomHarvList$no >= 8000], "\\",
-      randomHarvList$filename[randomHarvList$no < 9000 & randomHarvList$no >= 8000] , sep="")
+      randomHarvList8000$filename[randomHarvList8000$no < 9000 & randomHarvList8000$no >= 8000], " C:\\\\cg2011counting\\",
+      randomHarvList8000$happyDirs[randomHarvList8000$no < 9000 & randomHarvList8000$no >= 8000], "\\",
+      randomHarvList8000$filename[randomHarvList8000$no < 9000 & randomHarvList8000$no >= 8000] , sep="")
 setwd("C:\\\\cg2011counting\\")
 write.table(moveFiles, file="moveFiles8000.bat", sep="", row.names=FALSE, col.names=FALSE, quote=FALSE) 
 #again, find the batch file and double-click to run it. it's slow.
