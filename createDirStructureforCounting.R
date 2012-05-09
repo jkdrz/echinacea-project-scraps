979977769646dce89f3d4f544689d1de05ad0454
Josh Drizin <josh+cbg@respectablename.com>
2012-05-09 10:02:57 -0500

    all set up for the 4000s. i'd like to refactor the code so i can set a batch number and output path, but i don't know that i can do it to the leftover happydirs  or importing those and cleaning them.

--- createDirStructureforCounting.R
+++ createDirStructureforCounting.R
@@ -66,17 +66,19 @@ write.table(moveFiles, file="moveFiles8000.bat", sep="", row.names=FALSE, col.na
 # 4000 batch
 #
 ###############################################################################
+#read in the old happy words list
+happy <- read.csv("C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.LeftoverHappyDirs.csv")
+happy <- happy$x[5:length(happy$x)] #tiny was already used, so drop the first 4 
+happyDirs <- as.character(happy)
+
 inb1and2 <- harvList$no < 5000 & harvList$no >= 4000
 h4000 <- harvList[inb1and2, ]
 
-#we'll add a few dummy directories, just in case. pull out 490 entries
-happyList <- sample(happy$lowercase, 230, replace=FALSE) #FIXME ###############
-happyDirs <- rep(happyList, each=6)
 
 # put together the vectors, keeping lets and nos, so i can actually write out files in batches
 
 h4000 <- h4000[sample(1: dim(h4000)[1]), ] #sample the dataframe. sample(h8000) won't work
-happyDirs4000 <- happyDirs[1:230]
+happyDirs4000 <- happyDirs[1:520]
 randomHarvList4000 <- data.frame(h4000, happyDirs4000)
 leftoverHappyDirs <- happyDirs[(length(randomHarvList4000$letnoHarv)+1):length(happyDirs)]
 
@@ -85,17 +87,16 @@ write.csv(randomHarvList4000, file="C:/Documents and Settings/jdrizin/My Documen
 write.csv(leftoverHappyDirs, file="C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/2011.CG1.LeftoverHappyDirs-40008000.csv")
 
 # create directory structure. only run this this first time.
-setwd("C:\\\\cg2011counting\\")
-dirCommands <- paste("mkdir ", "C:\\cg2011counting\\", unique(randomHarvList4000$happyDirs4000), sep="")
+setwd("C:\\\\pelican\\")
+dirCommands <- paste("mkdir ", "C:\\pelican\\", unique(randomHarvList4000$happyDirs4000), sep="")
 write.table(dirCommands, file="mkdirCommands.bat", sep="", row.names=FALSE, col.names=FALSE, quote=FALSE) 
 #this writes hard paths. find the .bat file and double-click it to run it.
 
 # move files around this uses hard paths, which is ugly, but i think it's the only way
 #put together the copy commands with hard paths. use subsets to do batches manually.
 moveFiles <- paste("copy ", "C:\\2011_scans_sorted\\4000\\", 
-                   randomHarvList8000$filename[randomHarvList8000$no < 5000 & randomHarvList8000$no >= 4000], " C:\\\\cg2011counting\\",
-                   randomHarvList8000$happyDirs[randomHarvList8000$no < 5000 & randomHarvList8000$no >= 4000], "\\",
-                   randomHarvList8000$filename[randomHarvList8000$no < 5000 & randomHarvList8000$no >= 4000] , sep="")
-setwd("C:\\\\cg2011counting\\")
+                   randomHarvList4000$filename[randomHarvList4000$no < 5000 & randomHarvList4000$no >= 4000], " C:\\\\pelican\\",
+                   randomHarvList4000$happyDirs[randomHarvList4000$no < 5000 & randomHarvList4000$no >= 4000], "\\",
+                   randomHarvList4000$filename[randomHarvList4000$no < 5000 & randomHarvList4000$no >= 4000] , sep="")
 write.table(moveFiles, file="moveFiles4000.bat", sep="", row.names=FALSE, col.names=FALSE, quote=FALSE) 
 #again, find the batch file and double-click to run it. it's slow.
