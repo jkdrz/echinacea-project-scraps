c53ebeddb86984a3d0a2ff13b5ae23d675252477
Josh Drizin <josh+cbg@respectablename.com>
2012-05-29 12:26:06 -0500

    this script finds the 8000s means for randomization. the code can be converted from line 23 on for the newer datasheets

---
+++ combineCountsForRandomizing.R
@@ -0,0 +1,26 @@
+#this script is used to combine the seperated cg counts in the 2011 8000 batch
+#other counts are on one datasheet, and don't need to be combined in this way
+
+#read in the data
+setwd("C:/Documents and Settings/jdrizin/My Documents/Dropbox/CGData/165_count/count2011/count2011_work/")
+de1 <- read.csv("2011.CG1.countData8000-de1.csv", na.strings="") #r seems to read
+de2 <- read.csv("2011.CG1.countData8000-de2.csv", na.strings="") #these in strangely
+de3 <- read.csv("2011.CG1.countData8000-de3.csv", na.strings="")
+
+#drop empty rows
+de1 <- de1[complete.cases(de1$happyDirs8000),]
+de2 <- de2[complete.cases(de2$happyDirs8000),]
+de3 <- de3[complete.cases(de3$happyDirs8000),]
+
+#drop extraneous vectors from the data.frame
+de1 <- subset(de1, select=-c(X,count.1,countInit.1,countDate.1,countNote.1))
+de2 <- subset(de2, select=-c(X,count.1,countInit.1,countDate.1,countNote.1))
+de3 <- subset(de3, select=-c(X,count.1,countInit.1,countDate.1,countNote.1))
+
+#merge the files into one dataframe
+count8000 <- merge(de1,de2, by="filename")
+count8000 <- merge(count8000, de3, by="filename")
+#pull out the counts so I can use rowMeans
+counts    <- data.frame(count8000$count.x, count8000$count.y, count8000$count)
+count8000$meanCount <- rowMeans(counts,na.rm=TRUE)
+count8000$randomCount <- round((1/6) * count8000$meanCount)
